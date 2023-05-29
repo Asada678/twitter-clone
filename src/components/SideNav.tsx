a149bf5ded322f31c36d58597bdeaa3922ed7a68
Asada678 <asada.tomoya0923@gmail.com>
2023-05-29 18:31:43 +0900

    profile page

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -22,7 +22,7 @@ export function SideNav() {
         </li>
         {user != null && (
           <li>
-            <Link href={`/profiles/${user.id}`}>
+            <Link href={`/profile/${user.id}`}>
               <IconHoverEffect>
                 <span className="flex  items-center gap-4">
                   <VscAccount className="h-8 w-8" />
