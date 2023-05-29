82d681a734b75fa139cb04610d1b1d8ad6e5a813
Asada678 <asada.tomoya0923@gmail.com>
2023-05-29 19:25:52 +0900

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import Link from "next/link";
 import NewTweetForm from "~/components/NewTweetForm";
 import { api } from "~/utils/api";
 import InfiniteTweetList from "~/components/InfiniteTweetList";
